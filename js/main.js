89fcab56242b4317e9558251369d8178569335a3
lucas cassiano da silva <53881105+lussil@users.noreply.github.com>
2021-04-15 00:44:08 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 100,            // Integer: Speed of the transition, in milliseconds
+    timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+    // Function: After callback
+});
+
+
+$(".rslides_portifolio").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+    pager: true,           // Boolean: Show pager, true or false
+    nav: false,             // Boolean: Show navigation, true or false
+       // Function: After callback
+  });
